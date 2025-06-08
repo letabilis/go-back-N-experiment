7df79ae454f35cfe8ce0df2df25e2ece2b12b634
letabilis <sahilvdowlani@outlook.com>
2025-06-08 14:33:41 -0400

    feat(stats): counting expected, sent, lost packets

--- protocolo_stopandwait_experimento_completo/versao_gbn/cliente_gbn.py
+++ protocolo_stopandwait_experimento_completo/versao_gbn/cliente_gbn.py
@@ -1,21 +1,27 @@
 import socket
 import argparse
+import os
+from math import ceil
 
 N = 16 
 TIMEOUT = 2
 BLOCK_SIZE = 512
 SERVER_IP = '10.0.0.2'
 PORT = 5000
+INPUT_FILE_PATH = 'input_02.txt'
 
 def main():
     global N
+    global INPUT_FILE_PATH
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.settimeout(TIMEOUT)
 
     sliding_window = []
     next_seq = current_seq = 0
 
-    with open('input.txt', 'rb') as file:
+    count_expected_packets = ceil(os.path.getsize(INPUT_FILE_PATH) / BLOCK_SIZE)
+    count_sent_packets = 0
+    with open(INPUT_FILE_PATH, 'rb') as file:
         next_data = file.read(BLOCK_SIZE)
 
         while True:
@@ -24,6 +30,7 @@ def main():
                 packet = seq_byte + next_data
                 sock.sendto(packet, (SERVER_IP, PORT))
                 sliding_window.append((next_seq, packet))
+                count_sent_packets += 1
                 print('Enviado quadro com seq={}'.format(next_seq))
                 next_seq += 1
 
@@ -47,6 +54,7 @@ def main():
                 print('Timeout, reenviando a partir de seq={}'.format(current_seq))
                 for seq, packet_data in sliding_window:
                     sock.sendto(packet_data, (SERVER_IP, PORT))
+                    count_sent_packets += 1
                     print('Reenviado quadro com seq={}'.format(seq))
 
     print('Transmissão finalizada.')
@@ -56,22 +64,29 @@ def main():
     while not acked:
         packet = next_seq.to_bytes(1, 'big')
         sock.sendto(packet, (SERVER_IP, PORT))
+        count_sent_packets += 1
         print('Enviado quadro de encerramento com seq={}'.format(next_seq))
         try:
             ack, _ = sock.recvfrom(1)
             if int.from_bytes(ack, 'big') == next_seq:
                 acked = True
                 print('ACK {} recebido para encerramento'.format(next_seq))
+                print('EXPECTED_PACKETS {}'.format(count_expected_packets))
+                print('SENT_PACKETS {}'.format(count_sent_packets))
+                print('LOST_PACKETS {}'.format(count_sent_packets - count_expected_packets))
         except socket.timeout:
             print('Timeout, reenviando encerramento')
 
     sock.close()
 
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-w', '--window', type=int, choices=[4,8,16], help='Escolha o tamanho da janela: 4, 8, 16', default=16)
+    parser.add_argument('-w', '--window', type=int, choices=[4,8,16], help='Escolha o tamanho da janela: 4, 8, 16', default=N)
+    parser.add_argument('-f', '--file', type=str, help='Forneca o path do arquivo de entrada', default=INPUT_FILE_PATH)
     args = parser.parse_args()
     N = args.window
+    INPUT_FILE_PATH = args.file
     main()
     
