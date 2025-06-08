7d7a07f2c4079316ea353e5cd25e02aec39f390d
letabilis <sahilvdowlani@outlook.com>
2025-06-08 14:34:36 -0400

    adjust(gbn_server): change(buffersize), detect(end_packet)

--- protocolo_stopandwait_experimento_completo/versao_gbn/servidor_gbn.py
+++ protocolo_stopandwait_experimento_completo/versao_gbn/servidor_gbn.py
@@ -2,19 +2,18 @@ import socket
 
 IP = '10.0.0.2'
 PORT = 5000
-BUFFER_SIZE = 1024
-WINDOW_SIZE = 4
+BUFFER_SIZE = 512 + 1 
 
 def main():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind((IP, PORT))
 
     expected_seq = 0
-    sliding_window = {}
 
     with open('output.txt', 'wb') as file:
         print('Servidor aguardando dados...')
-        while True:
+        encerramento = False
+        while not encerramento:
             try:
                 data, addr = sock.recvfrom(BUFFER_SIZE)
                 seq = data[0]
@@ -26,16 +25,15 @@ def main():
                     expected_seq += 1
 
                 elif seq > expected_seq:
-                    sliding_window[seq] = payload
-                    print('Pacote seq={} armazenado na janela.'.format(seq))
+                    print('Descartado seq={}, espera-se seq={}.'.format(seq, expected_seq))
 
                 ack = bytes([expected_seq - 1])
                 sock.sendto(ack, addr)
                 print('ACK {} enviado.'.format(expected_seq - 1))
 
-            except Exception as e:
-                print('Erro: {}'.format(e))
-                break
+            except IndexError as e:
+                print('Recebido sinal de encerramento, payload é nulo')
+                encerramento = True
 
     sock.close()
 
